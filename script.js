ac49a727416702f87ba4e3010a1ba8755f3dc8fd
Cgotts <cobi.gottschalk@gmail.com>
2022-11-23 10:26:26 -0800

    Added program and profile pages

--- script.js
+++ script.js
@@ -1,10 +1,28 @@
-let dateEl = document.querySelector("#date")
+const dateEl = document.querySelector("#date")
+const programEl = document.querySelector('#program')
+const profileEl = document.querySelector('#profile')
 
 
 
+
+
+//Sets current date 
 setInterval(function () {
     var time = moment().format("dddd, h:mm:ss a");
     dateEl.textContent = time;
   }, 1000);
 
   
+
+  //Opens Program page
+  const programPage = () =>{
+window.open("/program.html")
+  }
+  programEl.addEventListener("click", programPage)
+
+
+// Opens Profile page
+  const profilePage = () =>{
+    window.open("/profile.html")
+      }
+      profileEl.addEventListener("click", profilePage)
