be13eb569fb59f28e8fc74d79603459cff99fd15
Cgotts <cobi.gottschalk@gmail.com>
2022-11-26 21:42:10 -0800

    Added other pages but they need tlc

--- program.js
+++ program.js
@@ -1,6 +1,12 @@
 let homepageBtn = document.querySelector("#homepage-btn")
+let customizeEl = document.querySelector(".customize")
 
 let homepageClick = ()=>{
     window.location.replace("./index.html")
       }
       homepageBtn.addEventListener("click", homepageClick)
+
+let customizeClick = () =>{
+  window.location.replace("./customize.html")
+}
+customizeEl.addEventListener("click", customizeClick)
